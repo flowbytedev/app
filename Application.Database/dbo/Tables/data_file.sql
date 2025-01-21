77c0fd88086872373fc25e388522316494319db8
Kevork Keheian <kevorkkeheian@gmail.com>
2025-01-21 16:18:28 +0200

    proj: add database project

---
+++ Application.Database/dbo/Tables/data_file.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[data_file] (
+    [id]          NVARCHAR (450) NOT NULL,
+    [file_name]   NVARCHAR (MAX) NULL,
+    [directory]   NVARCHAR (MAX) NULL,
+    [description] NVARCHAR (MAX) NULL,
+    [tags]        NVARCHAR (MAX) NULL,
+    CONSTRAINT [PK_data_file] PRIMARY KEY CLUSTERED ([id] ASC)
+);
+
---
+++ Application.Database/dbo/Tables/field_mapping.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE [dbo].[field_mapping] (
+    [source_table]          NVARCHAR (450) NOT NULL,
+    [source_column]         NVARCHAR (450) NOT NULL,
+    [destination_table]     NVARCHAR (450) NOT NULL,
+    [destination_column]    NVARCHAR (450) NOT NULL,
+    [source_data_type]      NVARCHAR (MAX) NOT NULL,
+    [destination_data_type] NVARCHAR (MAX) NOT NULL,
+    [is_group_by]           BIT            NULL,
+    [is_sum]                BIT            NULL,
+    [is_count]              BIT            NULL,
+    [filter_query]          NVARCHAR (MAX) NULL,
+    [default_value]         NVARCHAR (MAX) NULL,
+    CONSTRAINT [PK_field_mapping] PRIMARY KEY CLUSTERED ([source_table] ASC, [source_column] ASC, [destination_table] ASC, [destination_column] ASC)
+);
+
