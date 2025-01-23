2ddefd68b1e0c55500ef6d3656e54f118a285205
Youssef Hamieh <Youssef.Hamieh@spinneys-lebanon.com>
2025-01-23 13:33:16 +0200

    table: update variant_option_value table - add new column - add primary and foreign keys - add clustered and non clustered indexes

--- Application.Database/dbo/Tables/variant_option_value.sql
+++ Application.Database/dbo/Tables/variant_option_value.sql
@@ -1,6 +1,19 @@
 ﻿CREATE TABLE [dbo].[variant_option_value]
 (
-	valueid INT IDENTITY(1,1) PRIMARY KEY,
-    optionid INT NOT NULL,
-    optionvalue NVARCHAR(50) NOT NULL, 
+    [company_id] NVARCHAR(10) NOT NULL, 
+    [item_no] NVARCHAR(255) NOT NULL, 
+    [option_name] NVARCHAR(255) NOT NULL, 
+    [value] NVARCHAR(255) NOT NULL, 
+    CONSTRAINT [PK_variant_option_value] PRIMARY KEY ([company_id], [item_no], [option_name]), 
+    CONSTRAINT [FK_variant_option_value_company] FOREIGN KEY ([company_id]) REFERENCES [company]([id]), 
+    CONSTRAINT [FK_variant_option_value_item] FOREIGN KEY ([company_id],[item_no]) REFERENCES [item]([company_id],[item_no]), 
+    CONSTRAINT [FK_variant_option_value_option_name] FOREIGN KEY ([company_id],[item_no],[option_name]) REFERENCES [variant_option]([company_id],[item_no],[name]) 
+    
 )
+GO
+
+CREATE INDEX [IX_variant_option_value_company_id_item] ON [dbo].[variant_option_value] ([company_id],[item_no])
+
+GO
+
+CREATE INDEX [IX_variant_option_value_option_name] ON [dbo].[variant_option_value] ([option_name])
