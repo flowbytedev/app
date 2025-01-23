b8b860af4201a87a3dcaba4827d62f2422bc785f
Youssef Hamieh <Youssef.Hamieh@spinneys-lebanon.com>
2025-01-23 13:31:59 +0200

    table: update variant_option table - add new columns - add primary and foreign keys - add clustered and non clustered indexes

--- Application.Database/dbo/Tables/variant_option.sql
+++ Application.Database/dbo/Tables/variant_option.sql
@@ -1,5 +1,12 @@
 ﻿CREATE TABLE [dbo].[variant_option] (
-    optionid INT IDENTITY(1,1) PRIMARY KEY,
-    itemnumber INT NOT NULL,
-    optname NVARCHAR(50) NOT NULL,
+    [company_id] NVARCHAR(10) NOT NULL, 
+    [item_no] NVARCHAR(255) NOT NULL, 
+    [name] NVARCHAR(255) NOT NULL, 
+    [description] NVARCHAR(MAX) NULL, 
+    CONSTRAINT [PK_variant_option] PRIMARY KEY NONCLUSTERED ([company_id], [item_no], [name]), 
+    CONSTRAINT [FK_variant_option_company] FOREIGN KEY ([company_id]) REFERENCES [company]([id]), 
+    CONSTRAINT [FK_variant_option_item] FOREIGN KEY ([company_id],[item_no]) REFERENCES [item]([company_id],[item_no]),
 )
+GO
+
+CREATE CLUSTERED INDEX [IX_variant_option_company_id_item] ON [dbo].[variant_option] ([company_id],[item_no])
