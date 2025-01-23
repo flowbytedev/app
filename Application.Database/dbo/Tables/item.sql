250766430e75bf5d49cb552a8e7c8330a662db8a
Youssef Hamieh <Youssef.Hamieh@spinneys-lebanon.com>
2025-01-23 13:28:57 +0200

    table: update item table - add new column - add primary and foreign keys - add clustered and non clustered indexes

--- Application.Database/dbo/Tables/item.sql
+++ Application.Database/dbo/Tables/item.sql
@@ -1,7 +1,20 @@
 ﻿CREATE TABLE [dbo].[item]
 (
-	item_no_ [nvarchar](20) NOT NULL, 
-    variant_code NVARCHAR(50) NOT NULL,
-    item_name NVARCHAR(50) NOT NULL 
+	[company_id] NVARCHAR(10) NOT NULL,
+    item_no [nvarchar](255) NOT NULL, 
+    [title] NVARCHAR(255) NULL,
+    [description] NVARCHAR(MAX) NULL, 
+    [brand] NVARCHAR(255) NULL, 
+    CONSTRAINT [PK_item] PRIMARY KEY ([company_id], [item_no]), 
+    CONSTRAINT [FK_item_company] FOREIGN KEY ([company_id]) REFERENCES [company]([id])     
+     
 )
 
+
+GO
+
+CREATE INDEX [IX_item_title] ON [dbo].[item] ([title])
+
+GO
+
+CREATE INDEX [IX_item_brand] ON [dbo].[item] ([brand])
