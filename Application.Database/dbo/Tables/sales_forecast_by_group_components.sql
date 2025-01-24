646000c0c009a4050d6993d9d79d96c5651d358a
Romy Bou Abdo <romy.bouabdo@gmrlgroup.com>
2025-01-24 17:04:46 +0200

    refactor: refactor store scheme to store gorup

--- Application.Database/dbo/Tables/sales_forecast_by_group_components.sql
+++ Application.Database/dbo/Tables/sales_forecast_by_group_components.sql
@@ -1,7 +1,7 @@
-﻿CREATE TABLE [dbo].[sales_forecast_by_scheme_components]
+﻿CREATE TABLE [dbo].[sales_forecast_by_group_components]
 (
 	[date] DATETIME NOT NULL,
-    [store_scheme] NVARCHAR(10) NOT NULL, 
+    [store_group] NVARCHAR(10) NOT NULL, 
     [model] NVARCHAR(255) NOT NULL, 
     [trend] DECIMAL(38, 20) NULL, 
     [trend_lower] DECIMAL(38, 20) NULL, 
@@ -16,9 +16,9 @@
     [extra_regressors_lower] DECIMAL(38, 20) NULL, 
     [extra_regressors_upper] DECIMAL(38, 20) NULL, 
     [specific_regressors] NVARCHAR(MAX) NULL, 
-    CONSTRAINT [PK_sales_forecast_by_scheme_components] PRIMARY KEY ([date], [store_scheme], [model])
+    CONSTRAINT [PK_sales_forecast_by_group_components] PRIMARY KEY NONCLUSTERED ([date], [store_group], [model])
 )
 
 GO
 
-CREATE CLUSTERED INDEX [IX_sales_forecast_by_scheme_components_Column] ON [dbo].[sales_forecast_by_scheme_components] ([date])
+CREATE CLUSTERED INDEX [IX_sales_forecast_by_group_components_Column] ON [dbo].[sales_forecast_by_group_components] ([date])
