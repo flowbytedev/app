2666f2d6e5b03590e199fa3520d8d58a6051cb6e
Kevork Keheian <kevorkkeheian@gmail.com>
2025-01-25 20:16:39 +0200

    model: add item classes

---
+++ Application.Shared/Models/Inventory/Item.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Shared.Models.Inventory;
+
+
+[PrimaryKey(nameof(CompanyId), nameof(Code))]
+public class Item : BaseModel
+{
+    public string? Code { get; set; }
+
+    public string? Description { get; set; }
+
+    public string? Brand { get; set; }
+    public string? SubBrand { get; set; }
+    public string? BaseUom { get; set; }
+
+    public string? DefaultVendorNo { get; set; }
+
+
+    //public List<Variant>? Variants { get; set; }
+    public List<VariantOption>? VariantOptions { get; set; }
+}
---
+++ Application.Shared/Models/Inventory/Variant.cs
@@ -0,0 +1,35 @@
+﻿using Application.Shared.Models.Sales;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Application.Shared.Models.Inventory;
+
+
+
+[PrimaryKey(nameof(CompanyId), nameof(ItemCode), nameof(VariantCode))]
+public class Variant : BaseModel
+{
+    public string ItemCode { get; set; }
+
+    public string VariantCode { get; set; }
+
+    public string VariantOptionName { get; set; }
+    
+    [JsonIgnore]
+    public VariantOption VariantOption { get; set; }
+
+    public string Value { get; set; }
+
+
+    // public Variant Variant { get; set; }
+    public List<VariantOption> VariantOptions { get; set; }
+
+    public List<SalesPrice> SalesPrices { get; set; }
+
+}
