c9802ea94bfa9c5aabcbccd31fbc5cbe5afa9440
Youssef Hamieh <Youssef.Hamieh@spinneys-lebanon.com>
2025-01-27 10:16:09 +0200

    table: create tables currency, customer_status, customer_type. Added respective foreign keyes

---
+++ Application.Database/dbo/Tables/customer_type.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[customer_type]
+(
+    [code] NVARCHAR(255) NOT NULL, 
+    [type] NVARCHAR(255) NOT NULL, 
+    [description] NVARCHAR(MAX) NULL, 
+    CONSTRAINT [PK_customer_type] PRIMARY KEY ([code]) 
+)
