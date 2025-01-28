8f1e323aed52d4d06fc8e97c1c15c11b139b8be2
Kevork Keheian <kevorkkeheian@gmail.com>
2025-01-28 19:25:27 +0200

    table: rename sales forecast by sales channel table

--- Application.Database/dbo/Tables/sales_forecast_by_sales_channel.sql
+++ Application.Database/dbo/Tables/sales_forecast_by_sales_channel.sql
@@ -1,4 +1,4 @@
-﻿CREATE TABLE [dbo].[sales_forecast_by_store]
+﻿CREATE TABLE [dbo].[sales_forecast_by_sales_channel]
 (
     [company_id] NVARCHAR(10) NOT NULL,
 	[date] DATETIME NOT NULL, 
@@ -6,15 +6,15 @@
     [net_amount_acy] DECIMAL(38, 20) NOT NULL, 
     [net_amount_acy_upper] DECIMAL(38, 20) NOT NULL, 
     [net_amount_acy_lower] DECIMAL(38, 20) NOT NULL, 
-    CONSTRAINT [PK_sales_forecast_by_store] PRIMARY KEY NONCLUSTERED ([company_id], [date], [sales_channel_code]), 
-    CONSTRAINT [FK_sales_forecast_by_store_store] FOREIGN KEY ([company_id], [sales_channel_code]) REFERENCES [sales_channel]([company_id], [code]) 
+    CONSTRAINT [PK_sales_forecast_by_sales_channel] PRIMARY KEY NONCLUSTERED ([company_id], [date], [sales_channel_code]), 
+    CONSTRAINT [FK_sales_forecast_by_sales_channel_sales_channel] FOREIGN KEY ([company_id], [sales_channel_code]) REFERENCES [sales_channel]([company_id], [code]) 
      
 )
 ON [FLOWBYTE_SALES];
 
 GO
 
-CREATE CLUSTERED INDEX [IX_sales_forecast_by_store_date] ON [dbo].[sales_forecast_by_store] ([date]) 
+CREATE CLUSTERED INDEX [IX_sales_forecast_by_store_date] ON [dbo].[sales_forecast_by_sales_channel] ([date]) 
 ON [FLOWBYTE_SALES];
 
 
