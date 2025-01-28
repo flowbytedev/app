89576c02fe5c622efca55c338f697135b8aef218
Kevork Keheian <kevorkkeheian@gmail.com>
2025-01-28 13:16:00 +0200

    col: change pk in sales forecast by store to non clustered

--- Application.Database/dbo/Tables/sales_forecast_by_group.sql
+++ Application.Database/dbo/Tables/sales_forecast_by_group.sql
@@ -1,16 +1,20 @@
 ﻿CREATE TABLE [dbo].[sales_forecast_by_group]
 (
-	 [date] DATETIME NOT NULL, 
+    [company_id] NVARCHAR(10) NOT NULL,
+	[date] DATETIME NOT NULL, 
     [store_group] NVARCHAR(10) NOT NULL, 
     [net_amount_acy] DECIMAL(38, 20) NOT NULL, 
     [net_amount_acy_upper] DECIMAL(38, 20) NOT NULL, 
     [net_amount_acy_lower] DECIMAL(38, 20) NOT NULL, 
-    CONSTRAINT [PK_sales_forecast_by_group] PRIMARY KEY NONCLUSTERED ([date], [store_group]) 
+    CONSTRAINT [PK_sales_forecast_by_group] PRIMARY KEY ([company_id], [date], [store_group]) 
+     
 )
 ON [FLOWBYTE_SALES];
 
 GO
 
-CREATE CLUSTERED INDEX [IX_sales_forecast_by_group_Column] ON [dbo].[sales_forecast_by_group] ([date]) 
+CREATE NONCLUSTERED INDEX [IX_sales_forecast_by_group_Column] ON [dbo].[sales_forecast_by_group] ([date]) 
 ON [FLOWBYTE_SALES];
 
+
+GO
