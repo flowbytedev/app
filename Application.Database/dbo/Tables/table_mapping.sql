568a3be1cd2cf647952cb8419e5e0c12390c4765
Kevork Keheian <kevorkkeheian@gmail.com>
2025-02-06 12:51:16 +0200

    table: add table_mapping table

---
+++ Application.Database/dbo/Tables/table_mapping.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[table_mapping]
+(
+	[source_host] NVARCHAR(255) NULL, 
+    [source_database] NVARCHAR(255) NULL,
+	[source_table] [nvarchar](255) NULL,
+	[source_api_endpoint] NVARCHAR(MAX) NULL, 
+	[destincation_host] NVARCHAR(255) NULL, 
+    [destination_database] NVARCHAR(255) NULL,
+	[destination_table] [nvarchar](255) NULL,
+	[destination_api_endpoint] NVARCHAR(MAX) NULL,
+	[query] [nvarchar](MAX) NULL,
+)
