6d28b279ea36b52db0f0d674a2fe31ce732031a9
Kevork Keheian <kevorkkeheian@gmail.com>
2025-02-11 16:53:51 +0200

    col: add cdc_key to item_attribute table

--- Application.Database/dbo/Tables/item_attribute.sql
+++ Application.Database/dbo/Tables/item_attribute.sql
@@ -4,6 +4,7 @@
     [item_no] NVARCHAR(255) NOT NULL, 
     [attribute] NVARCHAR(255) NOT NULL, 
     [value] NVARCHAR(MAX) NOT NULL, 
+    [cdc_key] BIGINT NOT NULL, 
     CONSTRAINT [PK_item_attribute] PRIMARY KEY ([company_id], [item_no], [attribute]), 
     CONSTRAINT [FK_item_attribute_company] FOREIGN KEY ([company_id]) REFERENCES [company]([id]), 
     CONSTRAINT [FK_item_attribute_item] FOREIGN KEY ([company_id],[item_no]) REFERENCES [item]([company_id], [item_no]) 
