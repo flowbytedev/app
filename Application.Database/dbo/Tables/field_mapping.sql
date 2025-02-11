b9cc90f8a358076e7bbb02a932138f0999078e00
Kevork Keheian <kevorkkeheian@gmail.com>
2025-02-11 11:38:24 +0200

    col: change bool to non nullables with default value 0

--- Application.Database/dbo/Tables/field_mapping.sql
+++ Application.Database/dbo/Tables/field_mapping.sql
@@ -9,14 +9,14 @@
 	[destination_column] [nvarchar](255) NOT NULL,
 	[source_data_type] [nvarchar](255) NOT NULL,
 	[destination_data_type] [nvarchar](255) NOT NULL,
-	[is_group_by] [bit] NULL,
-	[is_sum] [bit] NULL,
-	[is_count] [bit] NULL,
+	[is_group_by] [bit] NOT NULL DEFAULT 0,
+	[is_sum] [bit] NOT NULL DEFAULT 0,
+	[is_count] [bit] NOT NULL DEFAULT 0,
 	[filter_query] [nvarchar](max) NULL,
 	[default_value] [nvarchar](max) NULL, 
-    [is_attribute] TINYINT NULL, 
-    [is_attribute_key] TINYINT NULL, 
-    [temp_table_name] NVARCHAR(255) NULL
+    [is_attribute] TINYINT NOT NULL DEFAULT 0, 
+    [is_attribute_key] TINYINT NOT NULL DEFAULT 0, 
+    [temp_table_name] NVARCHAR(255) NULL 
 	 
      
 ) ON [FLOWBYTE_DIM] TEXTIMAGE_ON [FLOWBYTE_DIM]
