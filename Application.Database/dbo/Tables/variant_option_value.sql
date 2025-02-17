44862bf1b75d1a9dd0bd888d4eddf410415d7366
Youssef Hamieh <Youssef.Hamieh@spinneys-lebanon.com>
2025-02-17 12:04:47 +0200

    col: add variant_code column

--- Application.Database/dbo/Tables/variant_option_value.sql
+++ Application.Database/dbo/Tables/variant_option_value.sql
@@ -3,12 +3,14 @@
     [cdc_key] BIGINT NULL,
     [company_id] NVARCHAR(10) NOT NULL, 
     [item_no] NVARCHAR(255) NOT NULL, 
+    [variant_code] NVARCHAR(255) NOT NULL,
+    [option_name] NVARCHAR(255) NOT NULL, 
     [status] INT NULL, 
     [description] NVARCHAR(255) NULL,
-    [option_name] NVARCHAR(255) NOT NULL, 
     [value] NVARCHAR(255) NOT NULL, 
      
      
+    
     CONSTRAINT [PK_variant_option_value] PRIMARY KEY ([company_id], [item_no], [option_name]), 
     CONSTRAINT [FK_variant_option_value_company] FOREIGN KEY ([company_id]) REFERENCES [company]([id]), 
     CONSTRAINT [FK_variant_option_value_item] FOREIGN KEY ([company_id],[item_no]) REFERENCES [item]([company_id],[item_no]), 
