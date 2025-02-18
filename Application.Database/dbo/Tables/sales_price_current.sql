a1311ba710407c29410b03be82bcc76a1b2a6208
Youssef Hamieh <Youssef.Hamieh@spinneys-lebanon.com>
2025-02-18 14:08:50 +0200

    table: rename to sales_price_current

--- Application.Database/dbo/Tables/sales_price_current.sql
+++ Application.Database/dbo/Tables/sales_price_current.sql
@@ -15,23 +15,23 @@
     
     
  
-    CONSTRAINT [FK_sales_price_company] FOREIGN KEY ([company_id]) REFERENCES [company]([id]), 
-    CONSTRAINT [FK_sales_price_item] FOREIGN KEY ([company_id],[item_no]) REFERENCES [item]([company_id],[item_no]), 
-    CONSTRAINT [FK_sales_price_currency] FOREIGN KEY ([currency_code]) REFERENCES [currency]([code]), 
+    CONSTRAINT [FK_sales_price_current_company] FOREIGN KEY ([company_id]) REFERENCES [company]([id]), 
+    CONSTRAINT [FK_sales_price_current_item] FOREIGN KEY ([company_id],[item_no]) REFERENCES [item]([company_id],[item_no]), 
+    CONSTRAINT [FK_sales_price_current_currency] FOREIGN KEY ([currency_code]) REFERENCES [currency]([code]), 
     CONSTRAINT [PK_sales_price_current] PRIMARY KEY NONCLUSTERED ([company_id],  [item_no], [variant_code], [sales_code], [currency_code],[unit_of_measure]) 
 )
 ON [FLOWBYTE_SALES];
 
 GO
 
-CREATE CLUSTERED INDEX [IX_sales_price_item_no_variant_code] ON [dbo].[sales_price_current] ([item_no],[variant_code])
+CREATE CLUSTERED INDEX [IX_sales_price_current_item_no_variant_code] ON [dbo].[sales_price_current] ([item_no],[variant_code])
 ON [FLOWBYTE_SALES];
 
 GO
 
 
-CREATE INDEX [IX_sales_price_company_id] ON [dbo].[sales_price_current] ([company_id])
+CREATE INDEX [IX_sales_price_current_company_id] ON [dbo].[sales_price_current] ([company_id])
 
 GO
 
-CREATE INDEX [IX_sales_price_sales_code] ON [dbo].[sales_price_current] ([sales_code])
+CREATE INDEX [IX_sales_price_current_sales_code] ON [dbo].[sales_price_current] ([sales_code])
