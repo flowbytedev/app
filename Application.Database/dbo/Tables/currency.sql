ebae2300f87ad9f22e094136e52cce824445baad
Kevork Keheian <kevorkkeheian@gmail.com>
2025-02-20 13:22:54 +0200

    proj: move database to separate repo flobytedev/db

--- Application.Database/dbo/Tables/currency.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE [dbo].[currency]
-( 
-    [code] NVARCHAR(10) NOT NULL, 
-    [name] NVARCHAR(255) NOT NULL, 
-    [description] NVARCHAR(MAX) NULL, 
-    CONSTRAINT [PK_currency] PRIMARY KEY ([code]), 
-    
-)
-ON [FLOWBYTE_DIM];
--- Application.Database/dbo/Tables/table.sql
+++
@@ -1,6 +0,0 @@
-﻿CREATE TABLE [dbo].[table] (
-    [schema] NVARCHAR (MAX) NOT NULL,
-    [name]   NVARCHAR (MAX) NOT NULL
-)
-ON [FLOWBYTE_DIM];
-
